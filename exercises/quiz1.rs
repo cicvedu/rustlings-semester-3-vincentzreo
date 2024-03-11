29650a6ad3f31bef4116d60712780864eb4e0630
zhouzhangqi <1978892854@qq.com>
2024-03-11 19:08:33 +0800

    update24/3/11/2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(val: usize) -> usize {
+    if val < 41 {
+       return val * 2;
+    } else {
+        return val;
+    }
+}
 
 // Don't modify this function!
 #[test]
